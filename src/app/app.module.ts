059e1eeb96b49193ba1b8cd519d8ff6fe5d38975
Floch <gaelle_floch@hotmail.com>
2020-11-19 16:51:11 +0100

    redirection 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,15 @@ import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AuthService} from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 
 const appRoutes: Routes = [
   {path : 'appareils', component: AppareilViewComponent},
   {path : 'appareils/:id', component: SingleAppareilComponent},
   {path : 'auth', component: AuthComponent},
-  {path: '', component: AppareilViewComponent}
+  {path: '', component: AppareilViewComponent},
+  {path: 'not-found', component: FourOhFourComponent},
+  {path: '**',redirectTo: '/not-found'}
 ];
 
 
@@ -32,6 +35,7 @@ const appRoutes: Routes = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
+    FourOhFourComponent,
 
   ],
   imports: [
