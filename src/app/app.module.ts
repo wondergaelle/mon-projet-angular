bdec10514c73768f066206ebf4e45cd7674ade4f
Floch <gaelle_floch@hotmail.com>
2020-11-19 14:51:31 +0100

    service authentification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {AppareilService} from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
 import {RouterModule, Routes} from '@angular/router';
+import {AuthService} from './services/auth.service';
 
 const appRoutes: Routes = [
   {path : 'appareils', component: AppareilViewComponent},
@@ -38,7 +39,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  authStatus: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
   }
 
+  ngOnInit(): void {
+    this.authStatus = this.authService.isAuth;
+  }
+
+  inSignIn() {
+    this.authService.signIn().then(
+      () => {
+        console.log('connexion réussie!');
+        this.authStatus = this.authService.isAuth;
+      }
+    );
+  }
+
+  onSignOut(){
+    this.authService.signOut();
+    this.authStatus = this.authService.isAuth;
+  }
 }
