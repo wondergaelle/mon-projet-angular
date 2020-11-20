c9e67a0cd21b4232d59a9fc56322c1e226c53f13
Floch <gaelle_floch@hotmail.com>
2020-11-20 14:55:52 +0100

    creation formulaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {AppareilService} from './services/appareil.service';
+
+
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,13 @@ import {AuthService} from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import {AuthGuard} from './services/auth-guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 
 const appRoutes: Routes = [
   {path : 'appareils', canActivate:[AuthGuard], component: AppareilViewComponent},
   {path : 'appareils/:id',canActivate:[AuthGuard], component: SingleAppareilComponent},
+  {path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent},
   {path : 'auth', component: AuthComponent},
   {path: '', component: AppareilViewComponent},
   {path: 'not-found', component: FourOhFourComponent},
@@ -38,6 +40,7 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
+    EditAppareilComponent,
 
   ],
   imports: [
